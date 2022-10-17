24782f69c8e2b8613cce345b39431f451fa58af0
a-real-human <19268@wc.school.nz>
2022-10-17 21:55:57 +1300

    lighting
    i added in a light source from the player

--- dtm assessment second one/Assets/scripts/FollowPlayer.cs
+++ dtm assessment second one/Assets/scripts/FollowPlayer.cs
@@ -6,12 +6,10 @@ public class FollowPlayer : MonoBehaviour
 {
     //what game object will be followed by the camera
     public GameObject player;
-    //to offset the camera so the player appears in the middle of the screen
-    private Vector3 offset = new Vector3(0, 0, -10);
     // Update is called once per frame
     void LateUpdate()
-    {   
+    {
         //follow player
-        transform.position = player.transform.position + offset;
+        transform.position = player.transform.position;
     }
 }
